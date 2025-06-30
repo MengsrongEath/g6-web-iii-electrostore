bc759a5a33eb015ad519d4c9a1b91022e6771752
Eath Mengsrong <athmengsrong2@gmail.com>
2025-06-30 23:59:47 +0700

    separate two files layout and section

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+		<title>Electro - HTML Ecommerce Template</title>
+
+		<!-- Styles -->
+		<?php require ("layouts/styles.php") ?>
+
+    </head>
+	<body>
+		<!-- HEADER -->
+		<?php require ("layouts/headers.php") ?>
+		<!-- /HEADER -->
+
+		<!-- NAVIGATION -->
+		<?php require ("layouts/navigations.php") ?>
+		<!-- /NAVIGATION -->
+
+		<main>
+			<?php require ("layouts/mains.php") ?>
+		</main>
+
+		<!-- FOOTER -->
+		<?php require ("layouts/footers.php") ?>
+		<!-- /FOOTER -->
+		 
+		<!-- jQuery Plugins -->
+		 <?php require ("layouts/script.php") ?>
+
+	</body>
+</html>
---
+++ layouts/mains.php
@@ -0,0 +1,43 @@
+<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<!-- shop-one -->
+					<?php require ("sections/shop-one.php") ?>
+					<!-- /shop-one -->
+
+					<!-- shop-two -->
+					<?php require ("sections/shop-two.php") ?>
+					<!-- /shop-two -->
+
+					<!-- shop-three -->
+					<?php require ("sections/shop-three.php") ?>
+					<!-- /shop-three -->
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+
+		<!--New Products SECTION -->
+		<?php require ("sections/newproducts.php") ?>
+		<!--/New Products SECTION -->
+
+		<!-- HOT DEAL SECTION -->
+		<?php require ("sections/hot-deal.php") ?>
+		<!-- /HOT DEAL SECTION -->
+
+		<!--Top-Selling SECTION -->
+		<?php require ("sections/top-selling.php") ?>
+		<!-- /SECTION -->
+
+		<!-- SECTION -->
+		<?php require ("sections/sup-top-selling.php") ?>
+		<!-- /SECTION -->
+
+		<!-- NEWSLETTER -->
+		<?php require ("sections/signup-for-newsells.php") ?>
+		<!-- /NEWSLETTER -->
